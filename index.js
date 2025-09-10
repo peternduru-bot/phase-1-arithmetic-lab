47ade20e87068dd7cadefc284f74f81d46fa2385
jones <pnduru6@gmail.com>
2025-09-10 10:14:11 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = 2;
+
+const num3 = 16;
+const num4 = 4;
+const mod = (num3 / num4);
+
+const max = 20;
